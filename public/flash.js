d74e8e60f5db521ca3f1a8c9b54b1e42cc2f2343
JusteUnCompteDeMerde <ruetschcorentin@gmail.com>
2026-06-11 19:39:10 +0200

    filigranne

--- public/flash.js
+++ public/flash.js
@@ -1,6 +1,6 @@
 const msgTexte = document.getElementById("msg-texte");
 
-msgTexte.addEventListener("click", () => {
+document.getElementById("message").addEventListener("click", () => {
     newText = prompt("Message (<br> pour sauter des lignes)", msgTexte.innerHTML);
     msgTexte.innerHTML = (newText == "" || newText == null) ? msgTexte.innerHTML : newText;
 })
